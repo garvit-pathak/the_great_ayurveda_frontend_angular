0addb7dc8ad747aefe201e33d19e6d9fb338eeaa
Divya <divyawaghmare2@gmail.com>
2022-05-12 15:45:00 +0530

    medicine service

--- src/app/medicines/medicines.component.ts
+++ src/app/medicines/medicines.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicineService } from '../service/medicine.service';
 
 @Component({
   selector: 'app-medicines',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medicines.component.css']
 })
 export class MedicinesComponent implements OnInit {
+medicines:any=[];
+keyword:string="";
+medicineList:any=[];
+  constructor(private medicineSearvice:MedicineService) {
+    this.medicineSearvice.viewMedicine().subscribe((data:any)=>{
+        console.log(data);
+        this.medicines=data;
+    })
+   }
+   public searchMedicine(keyword:string){
+       this.medicineSearvice.searchMedicine(keyword).subscribe((data:any)=>{
+         console.log(data);
+         this.medicineList=data;
+       })
+   }
 
-  constructor() { }
+   
 
   ngOnInit(): void {
   }
