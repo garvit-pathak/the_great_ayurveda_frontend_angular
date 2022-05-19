45bcf9db1407f690c46323559698c5ac1e15631b
faizan-lala <faizankhaan6267448042@gmail.com>
2022-05-19 17:45:12 +0530

    jayu2

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../service/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,9 +10,9 @@ import { Router } from '@angular/router';
 })
 export class UserProfileComponent implements OnInit {
  userDetail : any;
-  constructor(private router :Router) { 
-    
-    
+ uid:any;
+  constructor(private router :Router,private user:UserService,private taoster: ToastrService,) { 
+    this.uid=localStorage.getItem("userId")
   }
 
 
@@ -23,6 +25,18 @@ export class UserProfileComponent implements OnInit {
   ngOnInit(): void {
     this.userDetail = JSON.parse(localStorage.getItem("user") || "");
 
+  }
+  remove(){
+    confirm("confirm")
+    this.user.removeUser(this.uid).subscribe(data=>{
+      console.log(data)
+      if(data)
+       this.taoster.success('remove success full');
+       localStorage.removeItem('token');
+    localStorage.removeItem("user");
+    sessionStorage.removeItem('userId');
+       this.router.navigate([""]);
+    })
   }
 
 }
