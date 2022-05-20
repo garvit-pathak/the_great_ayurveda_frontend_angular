798dd491d5ee70bc7c2f31afa535cfef89582614
garvit-pathak <95516960+garvit-pathak@users.noreply.github.com>
2022-05-20 12:39:56 +0530

    changes

--- src/app/user-dashboard/diseases/diseases.component.ts
+++ src/app/user-dashboard/diseases/diseases.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { DoctorService } from 'src/app/service/doctor.service';
 import { DiseasesService } from '../../service/diseases.service';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-diseases',
@@ -12,26 +13,31 @@ export class DiseasesComponent implements OnInit {
   keywords = '';
   disList: any = [];
   doctor: any[] = [];
-  show : boolean = false;
-link:string='';
-  constructor(private diseasesService: DiseasesService ,private activatedRouter: ActivatedRoute,private router :Router,private drService : DoctorService) {
-this.router.events.subscribe(event=>{
-  this.keywords = <string> this.activatedRouter.snapshot.paramMap.get('search');
-  if(event instanceof NavigationEnd){
-    this.diseasesService.search(this.keywords).subscribe(data=>{
-      this.disList = data;
-      console.log(this.disList);
-      if(data.yogaLink){
-         this.link = this.disList.yogaLink 
-         this.show = true ;
-         console.log(this.link);
+  medicines: any[] = [];
+  show: boolean = false;
+  link: string = '';
+  constructor(private spinner: NgxSpinnerService, private diseasesService: DiseasesService, private activatedRouter: ActivatedRoute, private router: Router, private drService: DoctorService) {
+    this.router.events.subscribe(event => {
+      this.keywords = <string>this.activatedRouter.snapshot.paramMap.get('search');
+      if (event instanceof NavigationEnd) {
+        this.diseasesService.search(this.keywords).subscribe(data => {
+          this.spinner.hide();
+
+          this.disList = data;
+          this.medicines = data.medicines;
+          console.log(this.medicines);
+          if (data.yogaLink) {
+            this.link = this.disList.yogaLink
+            this.show = true;
+            console.log(this.link);
+
+          }
+        })
       }
-    })
-  }
-}
+    }
 
 
-)
+    )
     // this.diseasesService.search(this.keywords).subscribe((data) => {
     //   console.log(data);
     //   this.disList = data;
@@ -39,10 +45,11 @@ this.router.events.subscribe(event=>{
   }
 
   ngOnInit(): void {
-    this.drService.view().subscribe(data=>{
-      this.doctor=data;
+    this.spinner.show();
+    this.drService.view().subscribe(data => {
+      this.doctor = data;
 
-    },err=>{
+    }, err => {
       console.log(err);
     })
 
