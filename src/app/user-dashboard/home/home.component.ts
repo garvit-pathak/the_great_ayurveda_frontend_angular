6197bbe8f6b4aa4e12b856dbb6007327946b919e
garvit-pathak <95516960+garvit-pathak@users.noreply.github.com>
2022-05-24 11:06:07 +0530

    alot changes

--- src/app/user-dashboard/home/home.component.ts
+++ src/app/user-dashboard/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,34 @@ search: any;
 
   ngOnInit(): void {
   }
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true ,
+    touchDrag: true ,
+    pullDrag: true,
+    autoplay:true,
+    dots: true,
+    navSpeed: 200,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
+
+
  public navigate(event:any){
    this.search = event.target.value ;
    this.router.navigate(['diseases',this.search]);
