a24cc64a9ee0572a35e0ef077cce7ac9f6976d3b
garvit-pathak <95516960+garvit-pathak@users.noreply.github.com>
2022-05-27 11:25:07 +0530

    changes

--- src/app/user-dashboard/header/header.component.ts
+++ src/app/user-dashboard/header/header.component.ts
@@ -80,26 +80,7 @@ export class HeaderComponent implements OnInit {
       this.catList = data;
     });
 
-    this.cart.cartView(this.uid1).subscribe((data) => {
-      this.cart1 = data;
-      this.tota = data.medicineList.length;
-      this.cart1 = this.cart1.medicineList;
-      for (let i = 0; i < this.cart1.length; i++) {
-        this.cart1[i].pPrice = this.cart1[i].price;
-        this.cart1[i].index = i;
-        this.cart1[i].pro_qty = 1;
-      }
-      // console.log(this.cart1);
-      localStorage.setItem('cart', JSON.stringify(this.cart1));
-
-      this.cartList = data.medicineList;
-      this.totalPrice = 0;
-      for (let element of this.cartList) {
-        this.totalPrice += element.price * 1;
-      }
-      // console.log(this.totalPrice);
-      // this.view()
-    });
+   
 
   }
   get(id:string) {
